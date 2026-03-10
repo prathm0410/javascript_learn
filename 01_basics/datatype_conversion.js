b4851d2f06f37082cd6f16152ea4c7feb178ba4c
Prathmesh Thakur <162008809+prathm0410@users.noreply.github.com>
2026-03-10 11:57:23 +0000

    conversion of datatypes

---
+++ 01_basics/datatype_conversion.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+let score2 ="4343abc"
+let value2InNumber = Number(score2)
+console.log(value2InNumber)
+
+
+let isLoggedIn = 123
+let isboool = Boolean(isLoggedIn)
+console.log(isboool)
+
+
+let someNumber = "hitesh"
+let newString = String(someNumber)
+console.log(someNumber)
+console.log(typeof(newString))
