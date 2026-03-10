bd3351fc046f1d63eead1e8a9ef839b58b2c7c9a
Prathmesh Thakur <162008809+prathm0410@users.noreply.github.com>
2026-03-10 10:47:08 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,3 @@
+"use strict"// this is used to treat all the js code as a newer version 
+console.log(typeof undefined)
+console.log(typeof null)
---
+++ 01_basics/variable.js
@@ -0,0 +1,11 @@
+const accountId=144553
+let accountEmail="pratham@google.com"
+var accountPassword="12345"
+accountCity="Delhi"
+accountEmail="pratha@google.com"// not recommended as a good practice 
+let accountState
+
+// accountId=2  // not allowed
+// console.log(accountId)
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
