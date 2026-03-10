63835cfa8f43ac2a939e356b0a5261aafb4d7bfa
Prathmesh Thakur <162008809+prathm0410@users.noreply.github.com>
2026-03-10 12:18:19 +0000

    comparision done

---
+++ 01_basics/comparision.js
@@ -0,0 +1,14 @@
+// console.log("2" > 1)
+// console.log("02" > 1)
+console.log(null == 0)
+console.log(null > 0)
+console.log(null >= 0) 
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined >= 0)
+
+
+// ===
+
+console.log("2" === 2)
+// here there is a strict comparision between the two where there is no conversion of the string to the number here only strict comparision is done where datatype is also compared 
