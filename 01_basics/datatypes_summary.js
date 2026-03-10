23bc1908b0185072cec16ff4c0c5944c63bbe2d3
Prathmesh Thakur <162008809+prathm0410@users.noreply.github.com>
2026-03-10 14:18:21 +0000

    summary_datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,33 @@
+// there are two types of datatypes 
+//primitive and non primitive  
+
+// primitive : string , number, boolean, null, undefined, Symbol, BigInt
+
+
+
+// reference type/ non primitive : Array, objects, functions
+// dynamically typed
+
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)  //here the importance of symbol itself is to set the value of each input unique different from the previos one 
+const bigNumber = 16545454546741879987445565n
+
+
+
+// reference type 
+
+const superHeroes =["shaktimaan", "hero", "daga"]
+let myObj={
+  name : "prathmesh",
+  age: 22
+}
+const myFunction=function(){
+
+}
+
+
+console.table([typeof(superHeroes),typeof(myObj),typeof(myFunction)])
