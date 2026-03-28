9c783a15c2f9f7725ba671b314714ebabfbd33ed
Prathmesh Thakur <162008809+prathm0410@users.noreply.github.com>
2026-03-28 20:52:42 +0000

    maths and maths_and_numbers

---
+++ 01_basics/maths_and_numbers.js
@@ -0,0 +1,20 @@
+const score = 1000;
+const balance = new Number(1000)
+console.log(balance);
+console.log(score);
+console.log(balance.toFixed(2));
+console.log(balance.toString().length);
+
+const another_number = 100000000;
+console.log(another_number.toLocaleString('en-IN'));
+console.log(Math.abs(-10));
+console.log(Math.round(7.225));
+console.log(Math.ceil(7.225));
+console.log(Math.floor(7.225));
+console.log(Math.min(2,5,5,78,67));
+console.log(Math.max(2,5,5,78,67));
+console.log(Math.random());
+const max = 35;
+const min = 10;
+console.log(Math.floor((Math.random(2,5,5,78,67)*(max-min+1))+min));// code to print random values between two numbers 
+
