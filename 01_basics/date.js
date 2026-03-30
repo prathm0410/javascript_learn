c37d00453652ac62ac573780ff93f50429709cdd
Prathmesh Thakur <162008809+prathm0410@users.noreply.github.com>
2026-03-30 04:11:35 +0000

    DATE2

--- 01_basics/date.js
+++ 01_basics/date.js
@@ -1,3 +1,9 @@
 let myDate = new Date()
 console.log(myDate.toDateString());
 console.log(myDate.toString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);// type of date is object 
+
+let createdDate = new Date(2023, 1,17);
+console.log(createdDate.toDateString());
+console.log(createdDate.toLocaleDateString());
