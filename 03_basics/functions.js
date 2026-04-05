fc5ad1ce04e1fc080f19409c93fb3c4d638b7222
Prathmesh Thakur <162008809+prathm0410@users.noreply.github.com>
2026-04-05 09:59:11 +0000

    functions working in js

---
+++ 03_basics/functions.js
@@ -0,0 +1,30 @@
+function sayMyName(){
+    console.log("p");
+    console.log("r");
+    console.log("a");
+    console.log("t");
+    console.log("h");
+    console.log("a");
+    console.log("m");
+}
+
+sayMyName();// execution of the function
+
+
+function addtwonumber(number1, number2){
+    console.log(number1+number2)
+}
+function addtwonumbers(number1, number2){
+    let result=number1+number2;
+    return result
+}
+addtwonumber(9,10);
+const res = addtwonumbers(10,12)
+console.log(res);
+
+function loginUserMessage(username="sam"){
+    return `${username} just logged in `
+}
+
+console.log(loginUserMessage("garvati"));// jab kuch bhi paas nahi karoge the result comes out to be undefined 
+ 
